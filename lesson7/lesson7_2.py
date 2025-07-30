43389e26c69dd114136a4a1a98dcf64499a1cc90
王宸謙 <sam980109@gmail.com>
2025-07-30 02:41:45 +0000

    我修改了1個檔

--- lesson7/lesson7_2.py
+++ lesson7/lesson7_2.py
@@ -1,1 +1,7 @@
-print("Hellon! Python!")
+import argparse
+
+parser = argparse.ArgumentParser(description="要求使用者輸入名子")
+parser.add_argument("name", help="請輸入姓名")
+args = parser.parse_args()
+
+print(f"您的姓名是:{args.name}")
